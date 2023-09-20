8fe99ccb07901d203b52ff829dc9716aec9b4e63
Joshua Lambert <joshua.lambert@lshtm.ac.uk>
2023-09-20 14:55:54 +0200

    initial commit

---
+++ R/temp_func.R
@@ -0,0 +1,12 @@
+#' Temporary function for R folder
+#'
+#' @param x Variable.
+#'
+#' @return No output, called for printing side-effect.
+#' @export
+#'
+#' @examples
+#' temp_func("a")
+temp_func <- function(x) {
+  print(x)
+}
